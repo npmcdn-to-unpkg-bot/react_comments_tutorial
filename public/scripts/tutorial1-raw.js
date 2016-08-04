0182ae253bd47e15988c5e61b67f0c2df455fc1c
Richard Watkins <richardwatkins@Richards-MacBook-Pro.local>
2016-08-04 11:07:18 +0100

    fetching from the server code not working

---
+++ public/scripts/tutorial1-raw.js
@@ -0,0 +1,46 @@
+// tutorial2.js
+
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke"> This is *another comment</Comment>
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        <h1>Comments</h1>
+      <div>
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
